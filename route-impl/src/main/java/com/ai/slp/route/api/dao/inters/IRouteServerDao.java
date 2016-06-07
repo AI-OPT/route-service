fd3c881722f03d2c7ab8cadb31723e0f68c480ac
ascrutae <ascrutae@gmail.com>
2016-06-07 16:04:07 +0800

    移动工程

--- route-impl/src/main/java/com/ai/slp/route/api/dao/inters/IRouteServerDao.java
+++
@@ -1,9 +0,0 @@
-package com.ai.slp.route.api.dao.inters;
-
-import com.ai.slp.route.api.entity.RouteServer;
-
-import java.sql.SQLException;
-
-public interface IRouteServerDao {
-    RouteServer queryRouteServerByServerId(String serverId) throws SQLException;
-}
