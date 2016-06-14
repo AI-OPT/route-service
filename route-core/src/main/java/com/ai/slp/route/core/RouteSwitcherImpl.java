a681ed4c4572354baf0672768786bd8c6e6883fe
ascrutae <ascrutae@gmail.com>
2016-06-14 10:22:54 +0800

    修复bug

--- route-core/src/main/java/com/ai/slp/route/core/RouteSwitcherImpl.java
+++ route-core/src/main/java/com/ai/slp/route/core/RouteSwitcherImpl.java
@@ -1,12 +1,19 @@
 package com.ai.slp.route.core;
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class RouteSwitcherImpl implements IRouteSwitcher {
+
+    private Logger logger = LogManager.getLogger(RouteSwitcherImpl.class);
+
     @Override
     public Route switchRoute(String tenantId, String groupId, String dataJson) {
         RouteGroup routeGroup = RouteGroup.load(tenantId, groupId);
         if (routeGroup == null) {
-            throw new RuntimeException("Failed to find the tenantId:" + tenantId + " groupId:" + groupId);
+            logger.error("Failed to find the tenantId:{}, groupId:{}", tenantId, groupId);
+            return null;
         }
 
         Route result = RouteSwitcher.switchRoute(routeGroup, dataJson);
