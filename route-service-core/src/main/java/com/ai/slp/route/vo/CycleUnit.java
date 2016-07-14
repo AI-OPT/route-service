7e1279c559bb68dd6044083fb5dc69124e709d61
liutong <liutong6053@163.com>
2016-07-14 10:57:14 +0800

    修改周期时间获取方法.

--- route-service-core/src/main/java/com/ai/slp/route/vo/CycleUnit.java
+++ route-service-core/src/main/java/com/ai/slp/route/vo/CycleUnit.java
@@ -25,45 +25,49 @@ public enum CycleUnit {
     public static Timestamp buildNextInvalidTimeStamp(Timestamp startDate, int value, CycleUnit unit) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(startDate);
+        calendar.setFirstDayOfWeek(Calendar.MONDAY);
         // 当前时间也算在周期内，所以需要自减
         value--;
         switch (unit) {
+            //天
             case DAY: {
-                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
-                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth + value);
+//                int dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+//                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth + value);
+                calendar.add(Calendar.DAY_OF_MONTH,value);
                 break;
             }
+            //周
             case WEEK: {
-                int weekOfYear = calendar.get(Calendar.WEEK_OF_YEAR);
-                calendar.set(Calendar.WEEK_OF_YEAR, weekOfYear + value);
+                calendar.add(Calendar.WEEK_OF_YEAR,value);
                 int initDay = calendar.getFirstDayOfWeek();
-                calendar.set(Calendar.DAY_OF_WEEK, initDay);
-                calendar.add(Calendar.DAY_OF_MONTH, 7);
+                calendar.set(Calendar.DAY_OF_WEEK, initDay);//设置为周开始时间
+                calendar.add(Calendar.DAY_OF_WEEK, 6);//加六天到周末
                 break;
             }
+            //月
             case MONTH: {
-                int month = calendar.get(Calendar.MONTH);
-                calendar.set(Calendar.MONTH, month + value);
-                calendar.add(Calendar.MONTH, 1);
-                calendar.set(Calendar.DATE, 1);
-                calendar.add(Calendar.DATE, -1);
+                calendar.add(Calendar.MONTH, value+1);
+                calendar.set(Calendar.DAY_OF_MONTH, 1);
+                calendar.add(Calendar.DAY_OF_MONTH, -1);
                 break;
             }
+            //季度
             case QUARTER: {
+                //获取当前所属季度
                 int quarterNumber = getQuarter(calendar.get(Calendar.MONTH));
+                //判断是否进行加年操作
                 calendar.add(Calendar.YEAR, (quarterNumber + value ) / 4);
-                calendar.set(Calendar.MONTH, getQuarterInMonth((quarterNumber + value) % 4));
-                calendar.add(Calendar.MONTH, 1);
-                calendar.set(Calendar.DATE, 1);
-                calendar.add(Calendar.DATE, -1);
+                //获取季度最后月份并加1,用户后续最后一天获取
+                calendar.set(Calendar.MONTH, getQuarterInMonth((quarterNumber + value) % 4)+1);
+                calendar.set(Calendar.DAY_OF_MONTH, 1);//设置月份的第一天
+                calendar.add(Calendar.DAY_OF_MONTH, -1);//获取月份的最后一天
                 break;
             }
+            //年
             case YEAR: {
-                calendar.setTime(startDate);
-                int year = calendar.get(Calendar.YEAR);
-                calendar.set(Calendar.YEAR, year + value);
-                calendar.set(Calendar.DAY_OF_MONTH, 31);
-                calendar.set(Calendar.MONTH, 11);
+                calendar.add(Calendar.YEAR, value);
+                calendar.set(Calendar.MONTH, Calendar.DECEMBER);//设置为一年最后一月
+                calendar.set(Calendar.DAY_OF_MONTH, 31);//设置12月最后一天
                 break;
             }
 
